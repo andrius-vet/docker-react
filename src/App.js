0ffaa18fd8c25668d0ed66808b8a05d7e49e9256
Andrius <andrius.vetrenka@telesoftas.com>
2022-05-11 14:30:47 +0300

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React updated second time
+          Learn React updated second time second
         </a>
       </header>
     </div>
